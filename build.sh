2f66e4cd77bafae88ade1fe3d787c06a2f7d7443
Matt Allan <matthew.james.allan@gmail.com>
2016-08-23 13:16:00 -0400

    try omitting the -i arg for linux sed

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -e
 git clone https://github.com/composer/composer.git
 cd composer
 composer install
-sed -i '' 's/#!\/usr\/bin\/env php/#!\/data\/data\/com.termux\/files\/usr\/bin\/env php/g;n' ./src/Composer/Compiler.php
+sed -i 's/#!\/usr\/bin\/env php/#!\/data\/data\/com.termux\/files\/usr\/bin\/env php/g;n' ./src/Composer/Compiler.php
 php ./bin/compile
 
