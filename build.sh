1c4f5049f3420717985c64a402b723663650c6ba
Matt Allan <matthew.james.allan@gmail.com>
2018-01-06 11:35:09 -0500

    Build a specific version

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 set -e
 git clone https://github.com/composer/composer.git
 cd composer
+git checkout tags/1.6.2
 composer install
 sed -i 's/#!\/usr\/bin\/env php/#!\/data\/data\/com.termux\/files\/usr\/bin\/env php/g;n' ./src/Composer/Compiler.php
 php ./bin/compile
